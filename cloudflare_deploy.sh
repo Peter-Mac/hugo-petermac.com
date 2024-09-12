eb653a831d0c0409a91c160af582b86aa1248576
Peter Mac Giollafhearga <peter@petermac.com>
2024-09-12 15:35:44 +1000

    updates to remove unused partials and fix pagefind

--- cloudflare_deploy.sh
+++ cloudflare_deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-# showing how the non-public `deploy.sh` file works...
+# ensure wrangler is installed locally with:
+# npm install wrangler --save-dev
+# ensure you're logged in with:
+# npx wrangler login
+# then execute this command (ensuring the project name matches one already created)
 npm_config_yes=true npx wrangler pages deploy public --project-name="hugo-petermac-com" --commit-dirty=true
