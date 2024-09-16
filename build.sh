6ca88d609cdc1d55a86327e0b6f00028ea8d254b
Peter Mac Giollafhearga <peter@petermac.com>
2024-09-16 13:11:03 +1000

    added minify as a build option

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -ex
 rm -rf public
 rm -rf static/pagefind && rm -rf static/_pagefind # kill prev PF index, if any
-hugo --printPathWarnings --printUnusedTemplates --logLevel debug --cleanDestinationDir --ignoreCache
+hugo --printPathWarnings --printUnusedTemplates --logLevel debug --cleanDestinationDir --ignoreCache --minify
 # mv public/posts/index.xml public/index-excerpts.xml
 # mv public/posts/index.json public/index-excerpts.json
 npm_config_yes=true npx pagefind@latest --site ./public --output-subdir pagefind
