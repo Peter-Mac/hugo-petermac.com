570ab8f71c24181f1050f3939fbe45ee8c6c9e59
Peter Mac Giollafhearga <peter@petermac.com>
2024-09-17 20:10:32 +1000

    more commentary

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # set an stop condition if anything goes wrong
 set -ex
+
+# nuke the build folder
 rm -rf public
-rm -rf static/pagefind && rm -rf static/_pagefind # kill prev PF index, if any
+
+# nuke the pagefind index folder - this will be repopulated shotly
+rm -rf static/pagefind && rm -rf static/_pagefind
 
 hugo --printPathWarnings --printUnusedTemplates --logLevel debug --cleanDestinationDir --ignoreCache --minify
-# mv public/posts/index.xml public/index-excerpts.xml
-# mv public/posts/index.json public/index-excerpts.json
+
+# The following command generates the pagefind index and places it in a subdir
+# of the public folder. This is added to the cbuild command for deployments
 npm_config_yes=true npx pagefind@latest --site ./public --output-subdir pagefind
