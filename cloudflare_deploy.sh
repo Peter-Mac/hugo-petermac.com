35275756483ee191ceb8381e64b7a42448cbbc8d
Peter Mac Giollafhearga <peter@petermac.com>
2025-07-23 09:58:49 +1000

    comments around node dependency as I'm using python

--- cloudflare_deploy.sh
+++ cloudflare_deploy.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+
+# Ensure an operational version of node is installed (using nvm as the version manager)
+# From the ~/projects/dotfiles project, execute the following command
+# make apps-install-nvm
+# then find out what's running locally with the following command
+# nvm ls
+# List available installs wit the following command
+# nvm ls-remote
+# once identified an install candidate, install with the following command
+# nvm install lts/jod
+# Then make sure it's in use with the following command
+# nvm use lts/jod
+
 # ensure wrangler is installed locally with:
 # npm install wrangler --save-dev
 # ensure you're logged in with:
